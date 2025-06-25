b59e236f842d3d29d765e3dd1c901e5044624d47
igachev <ivophoenix1135@gmail.com>
2025-06-25 17:43:27 +0300

    fix: removed empty line in file course-info.ts

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -10,17 +10,14 @@ import { Course } from '../courses/courses.component';
 export class CourseInfoComponent implements OnInit {
   // Use the names for the input `course`.
     @Input() course!: Course
-
     @Input() showCourseInfo!: boolean
     @Output() hideCourseEmitter = new EventEmitter<boolean>()
 
      ngOnInit(): void {
-
       this.course.authors = this.course.authors.map((authorId) => {
         const author = mockedAuthorsList.find((author) => author.id === authorId);
         return author?.name as string
       });  
-        
       }
 
     hideCourse() {
